b82adcb10a63e402265d30583903a6fa8ed66c3d
Gordon06060 <gordonz0913@gmail.com>
2025-06-11 19:16:56 -0400

    fix: fullscreen map

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,5 @@
-import MapBox from './Components/MapBox';
+import MapBox from "./Components/MapBox";
 
 export default function Home() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <MapBox width="800px" height="550px" />
-    </div>
-  );
+  return <MapBox />;
 }
