4ca6747b226dd53d09b7c2a169dfcb2b4db4b823
FlameAndFlash <h2wiens@uwaterloo.ca>
2025-07-02 19:05:12 -0400

    Final design for AddOutlet

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,27 @@
-import Header from "./Components/Header";
-import MapBox from "./Components/MapBox";
-import AddOutlet from "./Components/AddOutlet";
+'use client';
+
+import { useState } from 'react';
+import MapBox from './Components/MapBox';
+import AddOutlet from './Components/AddOutlet';
 
 export default function Home() {
-  return <div className="relative w-full h-screen">
-    (
-    <>
-      <Header />
-      <MapBox />
-    </>
-  )
-    <AddOutlet />
-  </div>;
+  const [showOverlay, setShowOverlay] = useState(false);
+    //displays last coordinates on pin drop overlay
+  const [coords, setCoords] = useState<{lng: number; lat: number} | null>(null);
+
+  return (
+    <div className="relative w-full h-screen">
+      <MapBox 
+        onPinDrop={(lat, lng) => {
+          setCoords({ lat, lng });
+          setShowOverlay(true);
+        }}
+      />
+      <AddOutlet 
+        showOverlay={showOverlay}
+        coords={coords}
+        onClose={() => setShowOverlay(false)}
+      />
+    </div>
+  );
 }
