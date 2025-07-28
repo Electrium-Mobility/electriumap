8fab5137a5419103548d7fdb64e38f4cd06374f7
rohansaha27 <rohan.saha027@gmail.com>
2025-07-28 17:05:22 -0400

    fix: search functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           setShowPinOverlay(true);
         }}
         lightMode={lightMode}
+        flyTo={flyToLocation}
       />
       <Overlay 
         showPinOverlay={showPinOverlay}
