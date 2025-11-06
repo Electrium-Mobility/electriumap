37e8fce4d3a5132a7eeca01c549e7e3f78843725
Chahana Reddy <chahanareddy18@gmail.com>
2025-11-05 23:49:39 -0500

    #122: Render Pins on Search (Viewport Sync)

--- frontend/src/app/api/readOutlets.ts
+++ frontend/src/app/api/readOutlets.ts
@@ -1,6 +1,5 @@
 import { db } from "../firebase/firebase";
-import { collection, getDocs } from "firebase/firestore";
-
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export const readOutlets = async () => {
   try {
@@ -19,5 +18,51 @@ export const readOutlets = async () => {
     return outlets;
   } catch (error) {
     console.error("Error reading outlets from database: ", error);
+    return [];
+  }
+}
+
+export const readOutletsByBounds = async ({ swLat, swLng, neLat, neLng }: {
+  swLat: number;
+  swLng: number;
+  neLat: number;
+  neLng: number;
+}) => {
+  try {
+    const outletsCollection = collection(db, "Outlets");
+    const querySnapshot = await getDocs(outletsCollection); //fetch all outlets and filter by bounds
+    
+    const outlets = querySnapshot.docs
+    //map each doc to an object with id and doc.data fields
+      .map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      //filter outlets within bounds
+      .filter((outlet: any) => {
+        const lat = outlet.latitude;
+        const lng = outlet.longitude;
+        
+        //check if outlet's latitude is within the bounds (sw < ne)
+        const withinLat = lat >= swLat && lat <= neLat;
+        
+        //check longitude bounds
+        let withinLng;
+        if (swLng <= neLng) { //normal case: between swLng and neLng
+          withinLng = lng >= swLng && lng <= neLng;
+        }
+        
+        else {
+          withinLng = lng >= swLng || lng <= neLng; //bounds span the date line (ex: swLng = 150, neLng = -150)
+        }
+        
+        return withinLat && withinLng; //return true (keep outlet) if both lat and lng are within bounds
+      });
+    
+    return outlets; //return outlets in viewport
+  } 
+  catch (error) {
+    console.error("Error reading outlets by bounds from database: ", error);
+    return []; //empty array
   }
 }
